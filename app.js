887c9fd0dfde7984efa2eb9af8565fc7eefd24a3
Aleksi Siitari <aleksi.siitari@aalto.fi>
2020-09-21 16:58:46 +0300

    added hardcoded responses to test the bot

--- app.js
+++ app.js
@@ -17,4 +17,18 @@ server.listen(port, hostname, () => {
 
 const bot = new TeleBot(config.TELEGRAM_BOT_TOKEN);
 
-bot.on('/start', (msg) => msg.reply.text('Welcome!'));
+bot.on('/kluuvi', (msg) => {
+  return msg.reply.photo('https://unisport.fi/sites/default/files/styles/content_image_l/public/media/images/Viikko%2038%20-%20Kluuvi.jpg?itok=7ne4C-jt/get');
+});
+bot.on('/kumpula', (msg) => {
+  return msg.reply.photo('https://unisport.fi/sites/default/files/styles/content_image_l/public/media/images/Viikko%2038%20-%20Kumpula.jpg?itok=gWAdCQja/get');
+});
+bot.on('/meilahti', (msg) => {
+  return msg.reply.photo('https://unisport.fi/sites/default/files/styles/content_image_l/public/media/images/Viikko%2038%20-%20Meilahti.jpg?itok=-jVdotjz/get');
+});
+bot.on('/otaniemi', (msg) => {
+  return msg.reply.photo('https://unisport.fi/sites/default/files/styles/content_image_l/public/media/images/Viikko%2038%20-%20Otaniemi.jpg?itok=0w9KWdQT/get');
+});
+bot.on('/toolo', (msg) => {
+  return msg.reply.photo('https://unisport.fi/sites/default/files/styles/content_image_l/public/media/images/Viikko%2038%20-%20T%C3%B6%C3%B6l%C3%B6.jpg?itok=NfR4-zlT/get');
+});
