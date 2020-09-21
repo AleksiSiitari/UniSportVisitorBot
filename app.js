90ad040e92cae8dc0f3789d4c421ef3543fc3a63
Aleksi Siitari <aleksi.siitari@aalto.fi>
2020-09-21 17:37:13 +0300

    get port from env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const TeleBot = require('telebot');
 
 const hostname = '0.0.0.0';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
