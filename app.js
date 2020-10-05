99f9376073a87ab1f9298b0502e6de4819b50e4a
Aleksi Siitari <aleksi.siitari@aalto.fi>
2020-10-05 22:13:06 +0300

    Find the visitor graphs just by url

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const http = require('http');
 const TeleBot = require('telebot');
-const axios = require('axios');
-const cheerio = require('cheerio');
 
 const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
-
-cache = {htmlData: undefined, lastUpdated: undefined};
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -18,63 +14,39 @@ server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-function isCacheAlive() {
-  if (cache.htmlData && cache.lastUpdated) {
-    timeDiff = new Date() - cache.lastUpdated;
-    if (timeDiff < 86400000) { //86400000 is 1 day in milliseconds
-      return true;
-    }
-  }
-  return false;
+// https://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php
+Date.prototype.getWeekNumber = function(){
+  var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+  var dayNum = d.getUTCDay() || 7;
+  d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+  var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+  return Math.ceil((((d - yearStart) / 86400000) + 1)/7)
 };
 
-async function getUnisportHTMLData() {
-  if (isCacheAlive()) {
-    return cache.htmlData;
-  }
-
-  let htmlData;
-  axios.get('https://unisport.fi/kavijamaarat')
-    .then(function (response) {
-      htmlData = response.data;
-      cache.htmlData = htmlData;
-      cache.lastUpdated = new Date();
-    })
-    .catch(function (error) {
-      // handle error
-    })
-    .then(function () {
-      // always executed
-    });
-
-  return htmlData;
-};
-
-function parseURL(htmlData) {
-  urls = {kluuvi: "", kumpula: "", meilahti: "", otaniemi: "", toolo: ""};
-  const $ = cheerio.load(htmlData);
-  // TODO: Parse the image urls here
-
-  return urls;
+function createImageURL(location) {
+  // TODO: The images are not usually updated in time so we would need a to fallback on previous weeks data
+  const currentWeek = (new Date).getWeekNumber();
+  let url = `https://unisport.fi/sites/default/files/styles/content_image_2xl/public/media/images/Viikko%20${currentWeek}%20-%20${location}.jpg`;
+  return url;
 };
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
 bot.on('/start', (msg) => msg.reply.text('Welcome!'));
 bot.on('/kluuvi', (msg) => {
-  return msg.reply.photo('https://unisport.fi/sites/default/files/styles/content_image_l/public/media/images/Viikko%2038%20-%20Kluuvi.jpg?itok=7ne4C-jt/get');
+  return msg.reply.photo(createImageURL("Kluuvi"));
 });
 bot.on('/kumpula', (msg) => {
-  return msg.reply.photo('https://unisport.fi/sites/default/files/styles/content_image_l/public/media/images/Viikko%2038%20-%20Kumpula.jpg?itok=gWAdCQja/get');
+  return msg.reply.photo(createImageURL("Kumpula"));
 });
 bot.on('/meilahti', (msg) => {
-  return msg.reply.photo('https://unisport.fi/sites/default/files/styles/content_image_l/public/media/images/Viikko%2038%20-%20Meilahti.jpg?itok=-jVdotjz/get');
+  return msg.reply.photo(createImageURL("Meilahti"));
 });
 bot.on('/otaniemi', (msg) => {
-  return msg.reply.photo('https://unisport.fi/sites/default/files/styles/content_image_l/public/media/images/Viikko%2038%20-%20Otaniemi.jpg?itok=0w9KWdQT/get');
+  return msg.reply.photo(createImageURL("Otaniemi"));
 });
 bot.on('/toolo', (msg) => {
-  return msg.reply.photo('https://unisport.fi/sites/default/files/styles/content_image_l/public/media/images/Viikko%2038%20-%20T%C3%B6%C3%B6l%C3%B6.jpg?itok=NfR4-zlT/get');
+  return msg.reply.photo(createImageURL("Töölö"));
 });
 
 bot.start();
