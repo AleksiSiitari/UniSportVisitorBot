ca1d1ea994e39f47a64098f097ace3d2bf8d089e
Aleksi Siitari <aleksi.siitari@aalto.fi>
2022-03-16 11:34:27 +0200

    fix: update urls and refactor code

--- app.js
+++ app.js
@@ -15,6 +15,17 @@ server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+const locations = {
+  'kluuvi': {'name': 'Kluuvi', 'mapLink': 'https://goo.gl/maps/NVoVkKrgP3RdPX3H9'},
+  'kumpula': {'name': 'Kumpula', 'mapLink': 'https://goo.gl/maps/qL9QJDmoCao5QC3RA'},
+  'meilahti': {'name': 'Meilahti', 'mapLink': 'https://goo.gl/maps/rBPzmgTxq19bxXvy7'},
+  'otaniemi': {'name': 'Otaniemi', 'mapLink': 'https://goo.gl/maps/dHomZdpwQh4Ccx1R9'},
+  'toolo': {'name': 'Töölö', 'mapLink': 'https://goo.gl/maps/xvdqyP29ZAWDa6ae7'},
+};
+
+const realtimeUrl = locationName => `https://embed.gymplus.fi/v2/update/${locationName}`
+const url = (locationName, weekNumber) => `https://www.unisport.fi/sites/default/files/styles/content_image_2xl/public/media/images/${encodeURI(locationName)}%20vko%20${weekNumber}.png`;
+
 // https://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php
 Date.prototype.getWeekNumber = function(){
   var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
@@ -24,66 +35,57 @@ Date.prototype.getWeekNumber = function(){
   return Math.ceil((((d - yearStart) / 86400000) + 1)/7)
 };
 
-async function createImageURL(location) {
+async function getRealtimeData(locationName) {
+  const response = await fetch(realtimeUrl(locationName));
+  const body = await response.json();
+  return body;
+};
+
+async function createImageURL(locationName) {
   const lastWeek = (new Date).getWeekNumber() - 1;
-  const url = `https://unisport.fi/sites/default/files/styles/content_image_2xl/public/media/images/Viikko%20${lastWeek}%20-%20${encodeURI(location)}.jpg`;
   
-  fetchResults = await fetch(url, { method: 'HEAD' })
+  fetchResults = await fetch(url(locationName, lastWeek), { method: 'HEAD' })
     .then(res => {
           if (res.ok) {
-              return {url: url, week: lastWeek};
+              return {url: url(locationName, lastWeek), week: lastWeek};
           } else {
-            const weekBeforeLastWeek = lastWeek - 1; //Try to fallback to previous weeks data
-            return {url: `https://unisport.fi/sites/default/files/styles/content_image_2xl/public/media/images/Viikko%20${weekBeforeLastWeek}%20-%20${location}.jpg`,
+            const weekBeforeLastWeek = lastWeek - 1;
+            return {url: url(locationName, weekBeforeLastWeek),
               week: weekBeforeLastWeek};
           }
       })
       .catch(err => {
-        const weekBeforeLastWeek = lastWeek - 1; //Try to fallback to previous weeks data
-        return {url: `https://unisport.fi/sites/default/files/styles/content_image_2xl/public/media/images/Viikko%20${weekBeforeLastWeek}%20-%20${location}.jpg`,
+        const weekBeforeLastWeek = lastWeek - 1;
+        return {url: url(locationName, weekBeforeLastWeek),
           week: weekBeforeLastWeek};
       });
 
   return fetchResults;
 };
 
+const reply = (msg, location) => {
+  createImageURL(location.name).then(result =>
+    msg.reply.photo(result.url, {parseMode: 'HTML', caption: `<a href="${location.mapLink}">${location.name}</a>, average number of visitors on week ${result.week}`})
+  ).catch(err => {
+    msg.reply.text('There was an error fetching the data', { asReply: true });
+  });
+};
+
+const replyWithRealtimeData = (msg, locationName) => {
+  const data = await getRealtimeData(locationName);
+  const { realtime, predictions } = data;
+
+  msg.reply.text(`At the gym now: ${realtime.total.object_count}/${realtime.total.estimated_capacity}.
+   Predicted capacity ${predictions[1].title}: ${predictions[1].percent}, ${predictions[2].title}: ${predictions[2].percent}`, { asReply: true });
+}
+
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
 bot.on('/start', (msg) => msg.reply.text('Welcome!'));
-bot.on('/kluuvi', (msg) => {
-  createImageURL("Kluuvi").then(result =>
-    msg.reply.photo(result.url, {parseMode: 'HTML', caption: `<a href="https://goo.gl/maps/NVoVkKrgP3RdPX3H9">Kluuvi</a>, average number of visitors on week ${result.week}`})
-  ).catch(err => {
-    msg.reply.text('There was an error fetching the data', { asReply: true });
-  });
-});
-bot.on('/kumpula', (msg) => {
-  createImageURL("Kumpula").then(result =>
-    msg.reply.photo(result.url, {parseMode: 'HTML', caption: `<a href="https://goo.gl/maps/qL9QJDmoCao5QC3RA">Kumpula</a>, average number of visitors on week ${result.week}`})
-  ).catch(err => {
-    msg.reply.text('There was an error fetching the data', { asReply: true });
-  });
-});
-bot.on('/meilahti', (msg) => {
-  createImageURL("Meilahti").then(result =>
-    msg.reply.photo(result.url, {parseMode: 'HTML', caption: `<a href="https://goo.gl/maps/rBPzmgTxq19bxXvy7">Meilahti</a>, average number of visitors on week ${result.week}`})
-  ).catch(err => {
-    msg.reply.text('There was an error fetching the data', { asReply: true });
-  });
-});
-bot.on('/otaniemi', (msg) => {
-  createImageURL("Otaniemi").then(result =>
-    msg.reply.photo(result.url, {parseMode: 'HTML', caption: `<a href="https://goo.gl/maps/dHomZdpwQh4Ccx1R9">Otaniemi</a>, average number of visitors on week ${result.week}`})
-  ).catch(err => {
-    msg.reply.text('There was an error fetching the data', { asReply: true });
-  });
-});
-bot.on('/toolo', (msg) => {
-  createImageURL("Töölö").then(result =>
-    msg.reply.photo(result.url, {parseMode: 'HTML', caption: `<a href="https://goo.gl/maps/xvdqyP29ZAWDa6ae7">Töölö</a>, average number of visitors on week ${result.week}`})
-  ).catch(err => {
-    msg.reply.text('There was an error fetching the data', { asReply: true });
-  });
-});
+bot.on('/kluuvi', (msg) => replyWithRealtimeData(msg, 'kluuvi'));
+bot.on('/kumpula', (msg) => reply(msg, locations['kumpula']));
+bot.on('/meilahti', (msg) => reply(msg, locations['meilahti']));
+bot.on('/otaniemi', (msg) => reply(msg, locations['otaniemi']));
+bot.on('/toolo', (msg) => reply(msg, locations['toolo']));
 
 bot.start();
